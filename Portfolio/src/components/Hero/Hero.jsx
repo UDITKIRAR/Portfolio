4fd9e5233178be7313aee32eacb1038d19529026
unknown <uditkirar02u@example.com>
2024-09-06 18:13:17 +0530

    portmodified

--- Portfolio/src/components/Hero/Hero.jsx
+++ Portfolio/src/components/Hero/Hero.jsx
@@ -7,6 +7,7 @@ export const Hero = () => {
       <div className={styles.content}>
          <h1 className={styles.title}>Hi, I'm Udit</h1>
          <p className={styles.about}>I design and Code simple yet beautiful websites.</p>
+        
          <a className={styles.contact} href="mailto:uditkirar02@gmail.com">Contact Me</a>
       </div>
       <img className={styles.img} src={getImageUrl("hero/heroImage1.png")} alt='none'></img>
